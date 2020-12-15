1f252cc2e538ef4ba81d8f8b97ad2ed75de48589
JetLennit <jetlennit@gmail.com>
2020-12-15 13:16:06 -0500

    started adding instructions (none added yet)

--- cpu.h
+++ cpu.h
@@ -6,11 +6,24 @@ class CPU {
     public:
         Bus *bus;
         //http://wiki.nesdev.com/w/index.php/CPU_registers
-        unsigned char A;
-        unsigned char X;
-        unsigned char Y;
-        unsigned short PC;
+        unsigned char A = 0;
+        unsigned char X = 0;
+        unsigned char Y = 0;
+        unsigned short PC = 0;
         unsigned char S = 0xFD;
         unsigned char P = 0x34;
+
+        void runOpcode(unsigned char op){
+            //check get instruction from opcode (will be removed later when i order opcodes)
+            OPCODE opcode;
+            for(int o = 0; o < (sizeof(opcode)/sizeof(opcode[0])); o++){
+                if(op == opcode[o].opcode){
+                    opln = opcode[o];
+                    break;
+                }
+            }
+
+            std::string inst = opcode.instruction;
+        }
 };
 
