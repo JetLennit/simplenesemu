423f57b6cf56bd1720db95b78aaf7979d38a4039
JetLennit <jetlennit@gmail.com>
2020-12-14 19:19:18 -0500

    added addressing modes to opcodes

--- cpu.h
+++ cpu.h
@@ -12,25 +12,5 @@ class CPU {
         unsigned short PC;
         unsigned char S = 0xFD;
         unsigned char P = 0x34;
-
-    private:
-        unsigned short getAddressMode(OPCODE opcode){
-            unsigned short addressmode = opcode.opcode & 0b00011100;
-            unsigned short modifier = opcode.opcode & 0b00000011;
-            if(modifier == 0b01){
-                
-            }
-            else if(modifier == 0b10){
-
-            }
-            else if(modifier == 0b00){
-                
-            }
-            else{
-                std::cerr << "Nonexistant opcode modifier" << std::endl;
-                return -1;
-            }
-            return -1;
-        }
 };
 
